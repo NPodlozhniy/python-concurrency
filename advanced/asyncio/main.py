3dafc5463deb4054590b7388f7ec185ae92ef157
Nikita Podlozhniy <43523651+NPodlozhniy@users.noreply.github.com>
2023-01-15 23:03:40 +0300

    Add Semaphore using to manage number of threads (#1)

--- advanced/asyncio/main.py
+++ advanced/asyncio/main.py
@@ -9,11 +9,11 @@ URLS = [
     "microsoft.com",
     "amazon.com",
     "netflix.com",
-]
+] * 10
 
 # fetch all of these recources concurrently
 
-async def async_get(domain:str):
+async def async_get(domain: str, semaphore: asyncio.locks.Semaphore):
     """
     STEP 1: Create a Worker
     """
@@ -21,17 +21,19 @@ async def async_get(domain:str):
     print(f"fetching {domain}", flush=True)
     loop = asyncio.get_event_loop() # fetch a loop
     future = loop.run_in_executor(None, requests.get, url) # create a future
-    response = await future # wait for the future to complete
-    print(f"got a response code {response.status_code}", flush=True)
+    async with semaphore: # have to use `async with` context support operation
+        response = await future # wait for the future to complete
+        print(f"got a response code {response.status_code}", flush=True)
 
 async def fetch(urls: list):
     """
     STEP 2: To run all workers and wait for them
     """
     tasks = []
+    semaphore = asyncio.Semaphore(value=6)
     for i, url in enumerate(URLS): # create a list of workers
         print(f"i am running number {i}", flush=True)
-        worker = async_get(url) # create a coroutine
+        worker = async_get(url, semaphore) # create a coroutine
         task = asyncio.create_task(worker) # (optional) create task from coroutine
         tasks.append(task) # append to list of tasks
     # wait for all
